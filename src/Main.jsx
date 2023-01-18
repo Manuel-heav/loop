ab64cf16c255ec35a73fed841622a41ca9cc269d
manuel-heav <manuelheav32@gmail.com>
2023-01-18 19:24:40 +0300

    working on tmdb api generated image ui

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,28 +1,26 @@
 import React, { useEffect, useState } from 'react'
 
 const Main = () => {
-
-    const [movies, setMovies] = useState([]);
+    const [image, setImage] = useState("");
     const api_key = "api_key=9f02a73a699c175c26914cfc4ef6968e"
     const base_url = "https://api.themoviedb.org/3"
     const base_img_url = "https://image.tmdb.org/t/p/w500"
     const API_URL = base_url + "/discover/movie?sort_by=popularity.desc&"+api_key 
 
-    const randomNum = Math.floor(Math.random() * movies.length);
     useEffect(()=>{
         fetch(API_URL)
         .then(res => res.json())
-        .then(data => setMovies(data.results))
+        .then(datas => (
+            datas.results.map(data => setImage(data.poster_path))
+        ))
+
     }, [API_URL]);
-    setTimeout(() => {
-        const backImg = base_img_url + movies[randomNum].backdrop_path;
-        console.log(backImg)
-    }, 1000)
 
-
+    const backImg = base_img_url + image;
+    
   return (  
     <div className="main">
-
+        <img src={backImg} alt="" />
     </div>
   )
 }
